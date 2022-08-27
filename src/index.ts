ea3efb7f063db07da83a675ef471a985674d3099
lucasbbs <lucasbbs@live.fr>
2022-08-27 13:29:20 -0300

    188 - A Class Method for Collections

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { Collection } from './models/Collection';
-import { User, UserProps } from './models/User';
+import { User } from './models/User';
 
-const collection = new Collection<User, UserProps>(
-  'http://localhost:3000/users',
-  (json: UserProps) => User.build(json)
-);
-
+const collection = User.buildCollection();
 collection.on('change', () => console.log(collection));
 
 collection.fetch();
