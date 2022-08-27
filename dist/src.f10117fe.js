e50c2d5b4956c8c7f34eb263b5408d85edcba8a3
lucasbbs <lucasbbs@live.fr>
2022-08-27 14:24:16 -0300

    194 - Binding Event Handlers

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -132,13 +132,47 @@ function () {
     this.parent = parent;
   }
 
+  UserForm.prototype.eventsMap = function () {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
+    };
+  };
+
+  UserForm.prototype.onHeaderHover = function () {
+    console.log('H1 was hovered');
+  };
+
+  UserForm.prototype.onButtonClick = function () {
+    console.log('Hi, there');
+  };
+
   UserForm.prototype.template = function () {
-    return "\n    <div>\n      <h1>User Form</h1>\n      <input type=\"text\">\n    </div>\n    ";
+    return "\n    <div>\n      <h1>User Form</h1>\n      <input type=\"text\">\n      <button>Click me</button>\n    </div>\n    ";
+  };
+
+  UserForm.prototype.bindEvents = function (fragment) {
+    var eventsMap = this.eventsMap();
+
+    var _loop_1 = function _loop_1(eventKey) {
+      var _a = eventKey.split(':'),
+          eventName = _a[0],
+          selector = _a[1];
+
+      fragment.querySelectorAll(selector).forEach(function (element) {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    };
+
+    for (var eventKey in eventsMap) {
+      _loop_1(eventKey);
+    }
   };
 
   UserForm.prototype.render = function () {
     var templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   };
 
